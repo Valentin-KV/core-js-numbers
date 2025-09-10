81607e22ff308c7b0e6d68418ade6ff111663c16
Valentin-KV <krivenko.vg@yandex.ru>
2025-09-10 14:56:39 +0300

    feat: added a few more tasks

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -285,8 +285,12 @@ function getFibonacciNumber(index) {
  *   10 => 55 // (1+2+3+...+10)
  *   1  => 1
  */
-function getSumToN(/* n */) {
-  throw new Error('Not implemented');
+function getSumToN(n) {
+  let res = 0;
+  for (let i = 1; i <= n; i += 1) {
+    res = i + res;
+  }
+  return res;
 }
 
 /**
@@ -300,8 +304,13 @@ function getSumToN(/* n */) {
  *   202 => 4  // (2+0+2)
  *   5   => 5  // 5
  */
-function getSumOfDigits(/* num */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(num) {
+  const digits = num.toString().split('').map(Number);
+  const number = digits.reduce(
+    (accumulator, current) => accumulator + current,
+    0
+  );
+  return number;
 }
 
 /**
@@ -315,8 +324,13 @@ function getSumOfDigits(/* num */) {
  *   16  => true
  *   15  => false
  */
-function isPowerOfTwo(/* num */) {
-  throw new Error('Not implemented');
+function isPowerOfTwo(num) {
+  if (num < 1) return false;
+  let res = num;
+  while (res % 2 === 0) {
+    res /= 2;
+  }
+  return res === 1;
 }
 
 /**
@@ -329,8 +343,8 @@ function isPowerOfTwo(/* num */) {
  *   0 => 0
  *   Math.PI / 2 => 1
  */
-function getSine(/* num */) {
-  throw new Error('Not implemented');
+function getSine(num) {
+  return Math.sin(num);
 }
 
 /**
